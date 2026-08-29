af037bb319a3ed9c7ff0670d0a1a235638e3eb3c
jeettan <jeetta3@gmail.com>
2026-08-29 16:20:31 +0700

    Fix: Connect to Redis client on module load

--- api/config/redis.js
+++ api/config/redis.js
@@ -1,9 +1,15 @@
 const redis = require('redis');
 
-const { REDISURL } = process.env
-
 const client = redis.createClient({
     url: process.env.REDIS_URL
-})
+});
 
-module.exports = client
+client.on('error', (err) => {
+    console.error('Redis Client Error:', err);
+});
+
+client.connect().catch((err) => {
+    console.error('Failed to connect to Redis:', err);
+});
+
+module.exports = client;
